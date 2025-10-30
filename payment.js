383827b0de42ac070df37ee587d68717bcba99bc
José Alzir Bruno Falcão <alzir@unifor.br>
2025-10-30 18:16:09 -0300

    Add files via upload

---
+++ payment.js
@@ -0,0 +1,18 @@
+const DATABASE_MOCK = {
+    EMPREGADOR: {
+        nome: 'TESTE',
+        endereco: 'TESTE',
+        cnpj: 146545645645,
+    },
+    FUNCIONARIO: {
+        cod: 545646,
+        nome: 'Teste',
+        funcao: 'Teste',
+        cbo: 6512,
+        salarioBruto: 10.000,
+    },
+    VENDAS: {
+        taxaPorVenda: 0.01,
+        quantidadeDeVendas: 20
+    },
+}
